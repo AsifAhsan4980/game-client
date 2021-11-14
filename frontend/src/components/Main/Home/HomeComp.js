918f6f34662aa9eb60cc8755d8ff5feeb09b9dd9
Asif Ahsan <56015056+AsifAhsan4980@users.noreply.github.com>
2021-11-14 14:47:37 +0600

    icon fix

--- frontend/src/components/Main/Home/HomeComp.js
+++ frontend/src/components/Main/Home/HomeComp.js
@@ -6,17 +6,12 @@ import GameCart from "./GameCart";
 const HomeComp = () => {
     return (
         <>
-            <div class="mx-auto top-nav"  data-v-791b20d9 >
+            <div class="mx-auto top-nav" data-v-791b20d9>
                 <div data-v-791b20d9>
                     <div data-v-791b20d9>
                         <section class="pt-5">
-                            <div class="container" >
-                                <div id="example" class="
-          home_banner_wrapper
-          rounded-r-[6px] rounded-l-[6px]
-          md:mt-0
-          md:rounded-none
-        ">
+                            <div class="container">
+                                <div id="example" class="home_banner_wrapper rounded-r-[6px] rounded-l-[6px] md:mt-0 md:rounded-none">
                                     <div class="block home_banner_and_category_wrapper">
                                         <div>
                                             <Carousol/>
@@ -25,7 +20,7 @@ const HomeComp = () => {
                                 </div>
                             </div>
                         </section>
-                        <section class="container my-10" style={{padding:'0 !important'}}>
+                        <section class="container my-10" style={{padding: '0 !important'}}>
                             <div class="flex items-center mb-4 justify-between flex-wrap">
                                 <h3 class="uppercase h_3">New Games</h3>
                                 <GameCart/>
