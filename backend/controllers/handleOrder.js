9b2edde133bc0b382ff9b79732ca55e429c51a48
Asif Ahsan <56015056+AsifAhsan4980@users.noreply.github.com>
2021-11-24 13:59:21 +0600

    update

---
+++ backend/controllers/handleOrder.js
@@ -0,0 +1,10 @@
+const ErrorResponse = require("../utils/errorResponse")
+
+exports.handleOrder = async (req, res, next) => {
+    try{
+
+    }
+    catch(err) {
+        return new ErrorResponse("something went wrong")
+    }
+}
--- backend/routes/purchase.js
+++ backend/routes/purchase.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const {createNewPurchase} = require('../controllers/purchase')
 const {protect} = require("../middleware/auth");
+const {handleOrder} = require("../controllers/handleOrder");
 
 const router = express.Router()
 
-router.route('/').post(createNewPurchase)
+router.route('/').post(createNewPurchase, handleOrder)
 
 module.exports = router
